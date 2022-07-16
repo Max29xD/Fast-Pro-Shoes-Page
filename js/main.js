762282c2a5200540f90fd25a586dfbe5b8f0575e
Max29xD <marcos291198@gmail.com>
2022-07-15 21:15:10 -0400

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 let nav = document.querySelector('.nav');
+let w = window;
 
-window.addEventListener('scroll', function (e) {
-	window.scrollY >= 105 ? nav.classList.add('active') : nav.classList.remove('active');
+function scrollNav() {
+	w.scrollY >= 105 ? nav.classList.add('active') : nav.classList.remove('active');
+}
+scrollNav();
+w.addEventListener('scroll', function (e) {
+	scrollNav();
 });
+
+let scrollUp = document.querySelector('.scrollup');
+
+scrollUp.addEventListener('click', () => {
+	w.scrollTo(0, 0);
+});
+
+let btnSidebar = document.querySelector('.nav__menu-img');
+let btnClose = document.querySelector('.nav__link-close');
+let btnLink = document.querySelectorAll('.nav__link-li-a');
+let sidebar = document.querySelector('.nav__link');
+
+btnSidebar.addEventListener('click', () => {
+	sidebar.classList.toggle('active');
+});
+btnClose.addEventListener('click', () => {
+	sidebar.classList.remove('active');
+});
+btnLink.forEach((index) =>
+	index.addEventListener('click', () => {
+		sidebar.classList.remove('active');
+	})
+);
